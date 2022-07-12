38abff20358a884cafaaa07541a29ea80f19fbb1
Emil <emil@webform.se>
2022-07-12 10:09:01 +0200

    Done with robofriends

---
+++ robofriends/src/containers/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import Cardlist from "../components/Cardlist";
+import SearchBox from "../components/SearchBox";
+import Scroll from '../components/scroll'
+
+import './App.css';
+
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state= {
+            robots: [],
+            searchField: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({robots: users}))
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchField: event.target.value})
+    }
+
+    render () {
+        const{robots, searchField} = this.state;
+        const filteredRobots = robots.filter(robot =>{
+            return robot.name.toLowerCase().includes(searchField.toLowerCase()) 
+        })
+        if( !robots.length) {
+            return <h1>Loading</h1>
+        }
+        else {
+            return (
+                <div 
+                className="tc">
+                <h1 className="f1">Robofriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <Scroll>
+                <Cardlist robots={filteredRobots}/>
+                </Scroll>
+                </div>
+            )
+        }
+    }
+}
+
+export default App;
