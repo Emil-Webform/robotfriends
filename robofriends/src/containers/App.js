bd6b8a90bedd68a35564ab5622e9d84604bff8d1
Emil <emil@webform.se>
2022-07-12 11:01:04 +0200

    changes

--- robofriends/src/containers/App.js
+++ robofriends/src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Cardlist from "../components/Cardlist";
 import SearchBox from "../components/SearchBox";
 import Scroll from '../components/scroll'
+import ErrorBoundry from "../components/errorBoundary";
 
 import './App.css';
 
@@ -40,7 +41,9 @@ class App extends Component {
                 <h1 className="f1">Robofriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <Scroll>
-                <Cardlist robots={filteredRobots}/>
+                    <ErrorBoundry>
+                        <Cardlist robots={filteredRobots}/>
+                    </ErrorBoundry>
                 </Scroll>
                 </div>
             )
